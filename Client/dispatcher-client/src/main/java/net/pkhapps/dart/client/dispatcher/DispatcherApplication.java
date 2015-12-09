a291aed78a556a59368abf0662e0f5eb63e20d0c
Petter Holmstroem <peholmst@abo.fi>
2015-12-09 18:33:27 +0200

    Added client stub

---
+++ Client/dispatcher-client/src/main/java/net/pkhapps/dart/client/dispatcher/DispatcherApplication.java
@@ -0,0 +1,19 @@
+package net.pkhapps.dart.client.dispatcher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by petterprivate on 09/12/15.
+ */
+public class DispatcherApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
